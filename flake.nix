4aad7242e83d58eff657baa5d9dd1ed9eddb4c62
Mishmish <mishmish@mishmish.dev>
2022-12-14 22:29:37 +0200

    nixos-hardware

--- flake.nix
+++ flake.nix
@@ -4,18 +4,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
   };
 
-  outputs = inputs @ { nixpkgs, home-manager, nix-doom-emacs, ... }: {
+  outputs = inputs @ { nixpkgs, nixos-hardware, home-manager, nix-doom-emacs, ... }: {
     nixosConfigurations.bonditpad = 
       nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./os-configuration.nix
+          nixos-hardware.nixosModules.lenovo-thinkpad-l14-intel
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
